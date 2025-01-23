6cd5e31604acf9e5107054f9850ad844a47b89f3
kalix <kalix@kalix.kalix.com>
2025-01-23 07:56:54 -0500

    docker

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -12,19 +12,37 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
 // Initialize Sequelize with PostgreSQL configuration
-const sequelize = new Sequelize({
-  dialect: "postgres", // Change dialect to "postgres"
-  host: "localhost", // Set the host of the PostgreSQL server
-  port: 5432, // Default port for PostgreSQL
-  username: process.env.PG_USER, // Set your PostgreSQL username
+// const sequelize = new Sequelize({
+//   dialect: "postgres", // Change dialect to "postgres"
+//   host: "server", // Set the host of the PostgreSQL server
+//   port: 5432, // Default port for PostgreSQL
+//   username: process.env.PG_USER, // Set your PostgreSQL username
+//   password: process.env.PG_PASSWORD, // Set your PostgreSQL password
+//   database: process.env.PG_DATABASE, // Set your PostgreSQL database name
+//   logging: false, // Disable SQL query logging
+//   dialectOptions: {
+//     // Additional options for PostgreSQL, like SSL, timeouts, etc.
+//     timeout: 10000, // Optional, if you want to set a connection timeout
+//   },
+// });
+
+let sequelize= new Sequelize({ 
+    username: process.env.PG_USER, // Set your PostgreSQL username
   password: process.env.PG_PASSWORD, // Set your PostgreSQL password
-  database: process.env.PG_DATABASE, // Set your PostgreSQL database name
-  logging: false, // Disable SQL query logging
-  dialectOptions: {
-    // Additional options for PostgreSQL, like SSL, timeouts, etc.
-    timeout: 10000, // Optional, if you want to set a connection timeout
-  },
-});
+  database: process.env.PG_DATABASE, //
+    
+      host:"postgres",
+      dialect: "postgres",
+      port: 5432, 
+      pool: {
+        max: 100,
+        min: 0,
+        idle: 200000,
+        acquire: 1000000,
+      },
+    }
+  );
+
 
 // Sync the database
 async function initializeDatabase() {
